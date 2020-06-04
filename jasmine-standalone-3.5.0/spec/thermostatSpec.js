893d0ccd8839a9c5964b63bc0b9f44d09115b7af
Jonathan Filar <jonathanfilar@hotmail.com>
2020-06-04 15:56:28 +0100

    Still blocked on that spec for max temp

--- jasmine-standalone-3.5.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.5.0/spec/thermostatSpec.js
@@ -21,12 +21,15 @@ it("minimum tempreature is 10 degrees", function(){
 });
 
 it("maximum temperature is 25 degrees if power saving mode is on", function(){
-  expect(function(){thermostat.up(5);}).toThrow("when power saving mode is on max tempreature is 25 degrees")
+  expect(function(){thermostat.up(6);}).toThrow("when power saving mode is on max temperature is 25 degrees")
+});
+
+it ('turns the power mode off', function(){
+  expect(thermostat._mode_is_now_off()).toBe(false);
 });
 
 it("maximum temperature is 32 degrees if power saving mode is off", function(){
-  this.mode1 = "off"
-  expect(function(){thermostat.up(13);}).toThrow("when power saving mode is off max tempreature is 32 degrees")
+  expect(function(){thermostat.up(13);}).toThrow("when power saving mode is off max temperature is 32 degrees")
 });
 
 // it("power saving mode is on", function(){
--- jasmine-standalone-3.5.0/src/thermostat.js
+++ jasmine-standalone-3.5.0/src/thermostat.js
@@ -1,7 +1,7 @@
 class Thermostat {
   constructor(){
     this.degrees = 20;
-    this.mode1 = "on";
+    this.mode_on = true;
     this.maximum_power_saving_mode = 25;
     this.no_power_saving_mode = 32;
   }
@@ -10,13 +10,13 @@ class Thermostat {
   }
   up(number){
     this.degrees = this.degrees + number
-    if(this.mode1 && this.degrees > this.maximum_power_saving_mode){
-      throw "when power saving mode is on max tempreature is 25 degrees"
+    if(this.mode_on == true && this.degrees > this.maximum_power_saving_mode){
+      throw "when power saving mode is on max temperature is 25 degrees";
     }
-    else if(this.mode1 = "off" && this.degrees > this.no_power_saving_mode){
-      throw "when power saving mode is off max tempreature is 32 degrees"
+    else if(this.mode_on == false && this.degrees > this.no_power_saving_mode){
+      throw "when power saving mode is off max temperature is 32 degrees";
     }
-    else{
+    else {
     return this.degrees;}
   }
   down(number){
@@ -27,8 +27,8 @@ class Thermostat {
     this.degrees = this.degrees - number
     return this.degrees;}
   }
-  mode(){
-    if(this.degrees < this.maximum_power_saving_mode)
-    return this.degrees
-      }
-  };
+
+  _mode_is_now_off(){
+    return this.mode_on === false;
+  }
+}
